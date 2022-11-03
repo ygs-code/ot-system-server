760a8a40dac1015134a668dcb0a636c15f9f60d2
姚观寿 <yaoguanshou@rainbowcn.com>
2022-11-03 18:20:15 +0800

    修改登录接口

--- app/bizMod/set/bizMod/user/service/index.js
+++ app/bizMod/set/bizMod/user/service/index.js
@@ -86,7 +86,6 @@ class Service {
   }
   // 登录
   static async login(ctx, next, parameter = {}) {
-    
     const { username: name, phone, password } = parameter;
     const { request, response, cookies } = ctx;
 
@@ -148,33 +147,6 @@ class Service {
         userInfo,
       };
     }
-  }
-
-  static async verifyCode(ctx, next, parameter = {}) {
-    const { username: name, phone, password } = parameter;
-    const { request, response, cookies } = ctx;
-
-    // console.log('parameter=',parameter)
-
-    var codeConfig = {
-      size: 5, // 验证码长度
-      ignoreChars: "0o1i", // 验证码字符中排除 0o1i
-      noise: 3, // 干扰线条的数量
-      height: 35,
-      width: 110,
-      fontSize: 40,
-      color: false, //字符将有不同的颜色而不是灰色，如果设置了背景选项为True
-      background: `#99CCCC`, //SVG图像的背景颜色
-    };
-    var captcha = svgCaptcha.create(codeConfig);
-    const { data: imageSvg, text } = captcha;
-    setVerifyCode(text, text);
-    console.log("verifyCode=", text);
-
-    //登录成功
-    return {
-      svg: imageSvg,
-    };
   }
 
   static async getVerifyCode(ctx, next, parameter = {}) {
