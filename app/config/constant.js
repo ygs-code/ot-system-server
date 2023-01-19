4f956b7ecd87325bbe09e10f5a1be6a2033ceeb6
yao guan shou <281113270@qq.com>
2023-01-19 23:41:35 +0800

    修改配置

--- app/config/constant.js
+++ app/config/constant.js
@@ -1,2 +1,2 @@
-export const tokenExpires = 60 * 60 * 1000; // 设置如果没有请求 60分钟token登录失效
+export const tokenExpires = 24 * 60 * 60 * 1000; // 设置如果没有请求 24*60分钟token登录失效
 export const port = 3100; // 服务器端口
